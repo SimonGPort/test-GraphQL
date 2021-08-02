917bcb85dc1191e1a471993c5fe6c191eb8515f5
SimonGPort <simon.giroux.portelance@gmail.com>
2021-08-02 18:05:28 -0400

    front end component

--- test-graphql-client/src/App.js
+++ test-graphql-client/src/App.js
@@ -1,11 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  HttpLink,
+  from,
+} from "@apollo/client";
+import GetUsers from "./Components/GetUsers";
+import Form from "./Components/Form";
+import { onError } from "@apollo/client/link/error";
+
+// Voir le tutoriel youtube : https://youtu.be/YyUWW04HwKY
+
+const errorLink = onError(({ graphqlErrors, networkError }) => {
+  if (graphqlErrors) {
+    graphqlErrors.map(({ message, location, path }) => {
+      alert(`Graphql error ${message}`);
+    });
+  }
+});
+
+const link = from([
+  errorLink,
+  new HttpLink({ uri: "http://localhost:6969/graphql" }),
+]);
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: link,
+});
+
+
 
 function App() {
   return (
-    <div>
-      GraphQL test
-    </div>
+    <ApolloProvider client={client}>
+      {" "}
+      <GetUsers />
+      {/* <Form /> */}
+    </ApolloProvider>
   );
 }
 
