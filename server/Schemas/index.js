6d7169d43f14fc6568a28a30dacdb3f79b1df6d1
SimonGPort <simon.giroux.portelance@gmail.com>
2021-08-02 00:11:21 -0400

    getUserById new query

---
+++ server/Schemas/index.js
@@ -0,0 +1,67 @@
+const graphql = require("graphql");
+const {
+  GraphQLObjectType,
+  GraphQLSchema,
+  GraphQLInt,
+  GraphQLString,
+  GraphQLList,
+} = graphql;
+const userData = require("../MOCK_DATA.json");
+const UserType = require("./TypeDefs/UserType");
+
+
+
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    getAllUsers: {
+      type: new GraphQLList(UserType),
+      args: { id: { type: GraphQLInt } },
+      resolve(parent, args) {
+        return userData;
+      },
+    },
+    getUserById: {
+        type: new GraphQLList(UserType),
+        args: { id: { type: GraphQLInt } },
+        resolve(parent, args) {
+            let user=userData.filter((elem)=>{
+                return elem.id===args.id})
+
+                // Il faut que le type du retour soit un Array, meme s'il y a qu'une seul valeur
+        return user;
+        },
+      },
+  
+  },
+  
+},
+);
+
+
+const Mutation = new GraphQLObjectType({
+  name: "Mutation",
+  fields: {
+    createUser: {
+      type: UserType,
+      args: {
+        firstName: { type: GraphQLString },
+        lastName: { type: GraphQLString },
+        email: { type: GraphQLString },
+        password: { type: GraphQLString },
+      },
+      resolve(parent, args) {
+        userData.push({
+          id: userData.length + 1,
+          firstName: args.firstName,
+          lastName: args.lastName,
+          email: args.email,
+          password: args.password,
+        });
+        return args;
+      },
+    },
+  },
+});
+
+module.exports = new GraphQLSchema({ query: RootQuery, mutation: Mutation });
--- server/server.js
+++ server/server.js
@@ -3,10 +3,16 @@ const app = express();
 const PORT = 6969;
 const { graphqlHTTP } = require("express-graphql");
 const schema = require("./Schemas/index");
-const cors = require("cors");
+// const cors = require("cors");
+const userData=require('./MOCK_DATA.json')
 
-app.use(cors());
+// VOIR LE TUTORIAL YOUTUBE : 
+// Part1: https://youtu.be/Dr2dDWzThK8
+// Part2: https://youtu.be/YyUWW04HwKY
+
+// app.use(cors());
 app.use(express.json());
+
 app.use(
   "/graphql",
   graphqlHTTP({
@@ -15,6 +21,6 @@ app.use(
   })
 );
 
-app.listen(4000, () => {
-  console.log("Server running on 4000");
+app.listen(PORT, () => {
+  console.log("Server running on "+PORT);
 });
